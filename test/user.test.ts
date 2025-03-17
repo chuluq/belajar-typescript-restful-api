b9bb7cd5f31dcbe4a0261f93760e9a5eb98ee281
chuluq <chuluq@arvis.id>
2025-03-17 16:53:24 +0700

    feat: add login user api

--- test/user.test.ts
+++ test/user.test.ts
@@ -35,3 +35,46 @@ describe("POST /api/users", () => {
         expect(response.body.data.password).toBeUndefined();
     });
 });
+
+describe("POST /api/users/login", () => {
+    beforeEach(async () => {
+        await UserTest.create();
+    });
+
+    afterEach(async () => {
+        await UserTest.delete();
+    });
+
+    it("should able to login", async () => {
+        const response = await supertest(web).post("/api/users/login").send({
+            username: "test",
+            password: "test",
+        });
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body.data.username).toBe("test");
+        expect(response.body.data.name).toBe("test");
+        expect(response.body.data.token).toBeDefined();
+    });
+
+    it("should reject login if username is wrong", async () => {
+        const response = await supertest(web).post("/api/users/login").send({
+            username: "salah",
+            password: "test",
+        });
+        logger.debug(response.body);
+        expect(response.status).toBe(401);
+        expect(response.body.errors).toBeDefined();
+    });
+
+    it("should reject login if password is wrong", async () => {
+        const response = await supertest(web).post("/api/users/login").send({
+            username: "test",
+            password: "salah",
+        });
+        logger.debug(response.body);
+        expect(response.status).toBe(401);
+        expect(response.body.errors).toBeDefined();
+    });
+});
