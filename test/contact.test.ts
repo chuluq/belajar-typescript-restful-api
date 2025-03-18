797d533a9b72fa4208d9c20b2e321a8aaa05ae8f
chuluq <chuluq@arvis.id>
2025-03-18 11:07:26 +0700

    feat: add update contact api

--- test/contact.test.ts
+++ test/contact.test.ts
@@ -77,3 +77,58 @@ describe("GET /api/contacts/:contactId", () => {
         expect(response.body.errors).toBeDefined();
     });
 });
+
+describe("PUT /api/contacts/:contactId", () => {
+    beforeEach(async () => {
+        await UserTest.create();
+        await ContactTest.create();
+    });
+
+    afterEach(async () => {
+        await ContactTest.deleteAll();
+        await UserTest.delete();
+    });
+
+    it("should be able to update contact", async () => {
+        const contact = await ContactTest.get();
+        const response = await supertest(web).put("/api/contacts/" + contact.id).set("X-API-TOKEN", "test").send({
+            first_name: "choirul",
+            last_name: "chuluq",
+            email: "chuluq@test.com",
+            phone: "080808008080"
+        });
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body.data.id).toBe(contact.id);
+        expect(response.body.data.first_name).toBe("choirul");
+        expect(response.body.data.last_name).toBe("chuluq");
+        expect(response.body.data.email).toBe("chuluq@test.com");
+        expect(response.body.data.phone).toBe("080808008080");
+    });
+
+    it("should reject update contact if data is invalid", async () => {
+        const contact = await ContactTest.get();
+        const response = await supertest(web).put("/api/contacts/" + contact.id).set("X-API-TOKEN", "test").send({
+            first_name: "",
+            last_name: "",
+            email: "",
+            phone: ""
+        });
+        logger.debug(response.body);
+        expect(response.status).toBe(400);
+        expect(response.body.errors).toBeDefined();
+    });
+
+    it("should reject update contact if contact is not found", async () => {
+        const contact = await ContactTest.get();
+        const response = await supertest(web).put("/api/contacts/" + (contact.id + 1)).set("X-API-TOKEN", "test").send({
+            first_name: "choirul",
+            last_name: "chuluq",
+            email: "chuluq@test.com",
+            phone: "080808008080"
+        });
+        logger.debug(response.body);
+        expect(response.status).toBe(404);
+        expect(response.body.errors).toBeDefined();
+    });
+});
