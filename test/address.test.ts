ba63a69d71fc07285ed0e0e7997c7f2d7f00129d
chuluq <chuluq@arvis.id>
2025-03-19 07:24:23 +0700

    feat: add create address api

---
+++ test/address.test.ts
@@ -0,0 +1,70 @@
+import {AddressTest, ContactTest, UserTest} from "./test-util";
+import supertest from "supertest";
+import {web} from "../src/application/web";
+import {logger} from "../src/application/logging";
+
+describe("POST /api/contacts/:contactId/addresses", () => {
+    beforeEach(async () => {
+        await UserTest.create();
+        await ContactTest.create();
+    });
+
+    afterEach(async () => {
+        await AddressTest.deleteAll();
+        await ContactTest.deleteAll();
+        await UserTest.delete();
+    });
+
+    it("should be able to create address", async () => {
+        const contact = await ContactTest.get();
+
+        const response = await supertest(web).post(`/api/contacts/${contact.id}/addresses`).set("X-API-TOKEN", "test").send({
+            street: "test",
+            city: "test",
+            province: "test",
+            country: "test",
+            postal_code: "2222"
+        });
+
+        logger.debug(response.body);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.data.id).toBeDefined();
+        expect(response.body.data.street).toBe("test");
+        expect(response.body.data.city).toBe("test");
+        expect(response.body.data.province).toBe("test");
+        expect(response.body.data.country).toBe("test");
+        expect(response.body.data.postal_code).toBe("2222");
+    });
+
+    it("should reject create address if request is invalid", async () => {
+        const contact = await ContactTest.get();
+
+        const response = await supertest(web).post(`/api/contacts/${contact.id}/addresses`).set("X-API-TOKEN", "test").send({
+            street: "test",
+            city: "test",
+            province: "test",
+            country: "",
+            postal_code: ""
+        });
+
+        logger.debug(response.body);
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors).toBeDefined();
+    });
+
+    it("should reject create address if address is not found", async () => {
+        const contact = await ContactTest.get();
+
+        const response = await supertest(web).post(`/api/contacts/${contact.id + 1}/addresses`).set("X-API-TOKEN", "test").send({
+            street: "test",
+            city: "test",
+            province: "test",
+            country: "test",
+            postal_code: "2222"
+        });
+
+        logger.debug(response.body);
+        expect(response.statusCode).toBe(404);
+        expect(response.body.errors).toBeDefined();
+    });
+});
